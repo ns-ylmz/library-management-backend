cf7dc45d864eb60c4c27b847b87b6682d4269e3c
ns-ylmz <hire.nsylmz@gmail.com>
2026-04-14 11:40:41 +0300

    chore: format codebase with prettier

--- src/repositories/borrowRepository.ts
+++ src/repositories/borrowRepository.ts
@@ -1,71 +1,71 @@
-import { prisma } from "@/lib/prisma";
+import { prisma } from '@/lib/prisma';
 
 export const borrowRepository = {
-  findActiveBorrowsByUserAndBook: async (userId: number, bookId: number) => {
-    return await prisma.borrow.findFirst({
-      where: {
-        userId,
-        bookId,
-        returnedAt: null,
-      },
-      select: {
-        id: true,
-        userId: true,
-        bookId: true,
-        borrowedAt: true,
-        returnedAt: true,
-        score: true,
-      },
-    });
-  },
+	findActiveBorrowsByUserAndBook: async (userId: number, bookId: number) => {
+		return await prisma.borrow.findFirst({
+			where: {
+				userId,
+				bookId,
+				returnedAt: null,
+			},
+			select: {
+				id: true,
+				userId: true,
+				bookId: true,
+				borrowedAt: true,
+				returnedAt: true,
+				score: true,
+			},
+		});
+	},
 
-  findActiveBorrowsByBookId: async (bookId: number) => {
-    return await prisma.borrow.findFirst({
-      where: {
-        bookId,
-        returnedAt: null,
-      },
-      select: {
-        id: true,
-        userId: true,
-        bookId: true,
-        borrowedAt: true,
-      },
-    });
-  },
+	findActiveBorrowsByBookId: async (bookId: number) => {
+		return await prisma.borrow.findFirst({
+			where: {
+				bookId,
+				returnedAt: null,
+			},
+			select: {
+				id: true,
+				userId: true,
+				bookId: true,
+				borrowedAt: true,
+			},
+		});
+	},
 
-  create: async (userId: number, bookId: number) => {
-    return await prisma.borrow.create({
-      data: {
-        userId,
-        bookId,
-      },
-      select: {
-        id: true,
-        userId: true,
-        bookId: true,
-        borrowedAt: true,
-        returnedAt: true,
-        score: true,
-      },
-    });
-  },
+	create: async (userId: number, bookId: number) => {
+		return await prisma.borrow.create({
+			data: {
+				userId,
+				bookId,
+			},
+			select: {
+				id: true,
+				userId: true,
+				bookId: true,
+				borrowedAt: true,
+				returnedAt: true,
+				score: true,
+			},
+		});
+	},
 
-  returnBorrow: async (id: number, score: number) => {
-    return await prisma.borrow.update({
-      where: { id },
-      data: {
-        returnedAt: new Date(),
-        score,
-      },
-      select: {
-        id: true,
-        userId: true,
-        bookId: true,
-        borrowedAt: true,
-        returnedAt: true,
-        score: true,
-      },
-    });
-  },
+	returnBorrow: async (id: number, score: number) => {
+		return await prisma.borrow.update({
+			where: { id },
+			data: {
+				returnedAt: new Date(),
+				score,
+			},
+			select: {
+				id: true,
+				userId: true,
+				bookId: true,
+				borrowedAt: true,
+				returnedAt: true,
+				score: true,
+			},
+		});
+	},
 };
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,68 +1,68 @@
 import { prisma } from '@/lib/prisma';
 
 export const userRepository = {
-  findAll: async () => {
-    return await prisma.user.findMany({
-      select: {
-        id: true,
-        name: true,
-      },
-      orderBy: {
-        id: 'asc',
-      }
-    });
-  },
+	findAll: async () => {
+		return await prisma.user.findMany({
+			select: {
+				id: true,
+				name: true,
+			},
+			orderBy: {
+				id: 'asc',
+			},
+		});
+	},
 
-  findById: async (id: number) => {
-    return await prisma.user.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        id: true,
-        name: true,
-      },
-    });
-  },
+	findById: async (id: number) => {
+		return await prisma.user.findUnique({
+			where: {
+				id,
+			},
+			select: {
+				id: true,
+				name: true,
+			},
+		});
+	},
 
-  findByIdWithBorrows: async (id: number) => {
-    return await prisma.user.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        id: true,
-        name: true,
-        borrows: {
-          select: {
-            id: true,
-            borrowedAt: true,
-            returnedAt: true,
-            score: true,
-            book: {
-              select: {
-                id: true,
-                name: true,
-              },
-            },
-          },
-          orderBy: {
-            borrowedAt: 'desc',
-          },
-        },
-      }
-    });
-  },
+	findByIdWithBorrows: async (id: number) => {
+		return await prisma.user.findUnique({
+			where: {
+				id,
+			},
+			select: {
+				id: true,
+				name: true,
+				borrows: {
+					select: {
+						id: true,
+						borrowedAt: true,
+						returnedAt: true,
+						score: true,
+						book: {
+							select: {
+								id: true,
+								name: true,
+							},
+						},
+					},
+					orderBy: {
+						borrowedAt: 'desc',
+					},
+				},
+			},
+		});
+	},
 
-  create: async (name: string) => {
-    return await prisma.user.create({
-      data: {
-        name,
-      },
-      select: {
-        id: true,
-        name: true,
-      },
-    });
-  },
+	create: async (name: string) => {
+		return await prisma.user.create({
+			data: {
+				name,
+			},
+			select: {
+				id: true,
+				name: true,
+			},
+		});
+	},
 };
